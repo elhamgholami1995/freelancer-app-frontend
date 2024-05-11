178d567cac35226ccc9548ca7dc194c37a9f8803
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-11 08:29:35 +0330

    add color to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 function App() {
-  return <h1 className="text-3xl font-bold">شروع پروژه</h1>;
+  return (
+    <h1 className="container xl:max-w-screen-xl text-3xl font-bold  bg-primary-500 text-primary-900">
+      شروع پروژه
+    </h1>
+  );
 }
 export default App;
