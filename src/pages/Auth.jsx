536998da34c601cdbc33ec5d0beae98eb37b2e74
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-12 08:33:54 +0330

    create sendOTPForm component and send form

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+import SendOTPForm from "../features/Authentication/SendOTPForm/";
+
+function Auth() {
+  return (
+    <div className="w-full sm:max-w-sm">
+      <SendOTPForm />
+    </div>
+  );
+}
+
+export default Auth;
+
+// step=> 1 2
+// send otp
+// check otp
