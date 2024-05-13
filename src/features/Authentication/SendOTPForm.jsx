ce2f442f359fe74acfaa1d4bdd476edd70237635
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-13 08:49:14 +0330

    send otphandler function

--- src/features/Authentication/SendOTPForm.jsx
+++ src/features/Authentication/SendOTPForm.jsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
+import toast from "react-hot-toast";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
+  // usequerry= get
+  // usemutaation = post,put,delete
+
   return (
     <div>
-      <form className="space-y-8">
+      <form className="space-y-8" onSubmit={sendOtpHandler}>
         <TextField
           name="phoneNumber"
           label="شماره تماس"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button className="btn btn--primary w-full">ارسال کد تایید</button>
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تایید
+        </button>
       </form>
     </div>
   );
