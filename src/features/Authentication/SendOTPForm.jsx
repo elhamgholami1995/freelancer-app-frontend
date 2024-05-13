9e4995366c01a999c08211895893fe9705603f0c
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-13 07:59:38 +0330

    create textfield component and checkotp component

--- src/features/Authentication/SendOTPForm.jsx
+++ src/features/Authentication/SendOTPForm.jsx
@@ -1,25 +1,18 @@
 import { useState } from "react";
+import TextField from "../../ui/TextField";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
   return (
     <div>
       <form className="space-y-8">
-        <div>
-          <label className="mb-1" htmlFor="phonenumber">
-            شماره موبایل
-          </label>
-          <input
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            id="phonenumber"
-            className="textField__input"
-            type="text"
-          />
-        </div>
-        <button className="btn btn--primary w-full">
-          ارسال کد تایید
-        </button>
+        <TextField
+          name="phoneNumber"
+          label="شماره تماس"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,13 @@
+import CheckOTPForm from "../features/Authentication/CheckOTPForm";
 import SendOTPForm from "../features/Authentication/SendOTPForm/";
 
 function Auth() {
   return (
-    <div className="w-full sm:max-w-sm">
-      <SendOTPForm />
+    <div className="flex justify-center pt-10">
+      <div className="w-full sm:max-w-sm">
+        <SendOTPForm />
+        {/* <CheckOTPForm /> */}
+      </div>
     </div>
   );
 }
