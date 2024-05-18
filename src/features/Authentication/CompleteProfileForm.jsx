5823e4040d0d0fb1ba2bdfaafe0478559f438c70
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-18 23:25:04 +0330

    push user after complete profile

--- src/features/Authentication/CompleteProfileForm.jsx
+++ src/features/Authentication/CompleteProfileForm.jsx
@@ -5,11 +5,13 @@ import { useMutation } from "@tanstack/react-query";
 import { completeProfile } from "../../services/authService";
 import toast from "react-hot-toast";
 import Loading from "../../ui/Loading";
+import { useNavigate } from "react-router-dom";
 
 function CompleteProfileForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
+  const navigate = useNavigate();
   const { mutateAsync, isPending } = useMutation({
     mutationFn: completeProfile,
   });
@@ -18,8 +20,14 @@ function CompleteProfileForm() {
     e.preventDefault();
     try {
       const { user, message } = await mutateAsync({ name, email, role });
-      console.log(user, message);
       toast.success(message);
+      if (user.status !== 2) {
+        navigate("/");
+        toast("پروفایل شما در انتظار تایید است.", { icon: "👏" });
+        return;
+      }
+      if (user.role == "OWNER") return navigate("/owner");
+      if (user.role == "FREELANCER") return navigate("/freelancer");
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <div>صفحه اصلی</div>;
 }
 
-export default Home
+export default Home;
