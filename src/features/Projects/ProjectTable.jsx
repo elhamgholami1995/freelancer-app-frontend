14ddddcab54b6c5390a7faf5842376f47b712084
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-05-29 08:30:01 +0330

    se utils function like truncateext.js,tpersianNumbers.js,toLocaldateShort.js

--- src/features/Projects/ProjectTable.jsx
+++ src/features/Projects/ProjectTable.jsx
@@ -1,5 +1,10 @@
 import Empty from "../../ui/Empty";
 import Loading from "../../ui/Loading";
+import {
+  toPersianNumbers,
+  toPersianNumbersWithComma,
+} from "../../utils/toPersianNumbers";
+import truncateText from "../../utils/trunncateText";
 import useOwnerProjects from "./useOwnerProjects";
 
 function ProjectTable() {
@@ -26,10 +31,10 @@ function ProjectTable() {
           {projects.map((project, index) => (
             <tr key={project._id}>
               <td>{index + 1}</td>
-              <td>{project.title}</td>
+              <td>{truncateText(project.title, 30)}</td>
               <td>{project.category.title}</td>
-              <td>{project.budget}</td>
-              <td>{project.deadline}</td>
+              <td>{toPersianNumbersWithComma(project.budget)}</td>
+              <td>{toLocalDateShort(project.deadline)}</td>
               <td>
                 <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
                   {project.tags.map((tag) => (
---
+++ src/utils/trunncateText.js
@@ -0,0 +1,4 @@
+export default function truncateText(srt, length) {
+  if (structuredClone.length < length) return str;
+  return str.slice(0, length) + "...";
+}
