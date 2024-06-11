2d56cc50d1504022efc4bc6325ebf9c90f5073ab
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-06-11 09:06:49 +0330

    set select options,tags and datepicker component for create new project form

--- src/features/Projects/CreateProjectForm.jsx
+++ src/features/Projects/CreateProjectForm.jsx
@@ -1,11 +1,17 @@
 import { useForm } from "react-hook-form";
 import TextField from "../../ui/TextField";
+import RHFSelect from "../../ui/RHFSelect";
+import { TagsInput } from "react-tag-input-component";
+import { useState } from "react";
+import DatePickerField from "../../ui/DatePickerField";
 function CreateProjectForm() {
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
+  const [tags, setTags] = useState([]);
+  const [date, setDate] = useState(new Date());
 
   const onSubmit = (data) => {
     console.log(data);
@@ -51,6 +57,23 @@ function CreateProjectForm() {
         }}
         errors={errors}
       />
+      <RHFSelect
+        label="دسته بندی"
+        name="category"
+        register={register}
+        options={[]}
+        required
+      />
+      <div>
+        <label className="mb-2 block text-secondary-700 ">تگ</label>
+        <TagsInput
+          value={tags}
+          onChange={setTags}
+          name="tags"
+          classNames={{}}
+        />
+      </div>
+      <DatePickerField date={date} setDate={setDate} label="ددلاین" />
       <button type="submit" className="btn btn--primary w-full">
         تایید
       </button>
---
+++ src/ui/DatePickerField.jsx
@@ -0,0 +1,21 @@
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+
+export default function DatePickerField({ label, date, setDate }) {
+  return (
+    <div>
+      <span className="mb-2 block text-secondary-700">{label}</span>
+      <DatePicker
+        containerClassName="w-full"
+        calendarPosition="bottom-center"
+        inputClass="textField__input"
+        value={date}
+        onChange={(data) => setDate(data)}
+        format="YYYY/MM/DD"
+        calendar={persian}
+        locale={persian_fa}
+      />
+    </div>
+  );
+}
