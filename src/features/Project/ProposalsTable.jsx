ed929c4a66d03afc187fe3d4d9259c7ff742b59c
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-07-01 23:58:54 +0330

    set proposal table

--- src/features/Project/ProposalsTable.jsx
+++ src/features/Project/ProposalsTable.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProposalRow from "./ProposalRow";
 
-function ProposalsTable() {
-  return <div>ProposalsTable</div>;
+function ProposalsTable({ proposals }) {
+  if (!proposals.length) return <Empty resourceName="پروپوزالی " />;
+  return (
+    <Table>
+      <Table.header>
+        <th>#</th>
+        <th>فریلنسر</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.header>
+      <Table.Body>
+        {projects.map((project, index) => (
+          <ProposalRow key={project._id} project={project} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
 }
 
 export default ProposalsTable;
