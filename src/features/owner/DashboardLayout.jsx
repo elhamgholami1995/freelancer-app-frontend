a8fada2f3abc51eb2d90d4c3b16dae0d022107d5
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-07-22 22:51:24 +0330

    set ownerdashboard and stat component

---
+++ src/features/owner/DashboardLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DashboardHeader from "./DashboardHeader";
+import Stats from "./Stats";
+import useOwnerProjects from "../Projects/useOwnerProjects";
+import Loading from "../../ui/Loading";
+
+function DashboardLayout() {
+  const { isLoading, projects } = useOwnerProjects();
+  if (isLoading) return <Loading />;
+  return (
+    <div>
+      <DashboardHeader />
+      <Stats projects={projects} />
+    </div>
+  );
+}
+
+export default DashboardLayout;
